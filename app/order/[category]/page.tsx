6f19b93a10258f0b2632d67df0f15480ee5deb97
JmcaDev <jmcavzla@gmail.com>
2025-01-27 20:32:10 -0400

    Adding products to the view

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import ProductCard from "@/components/products/ProductCard"
+import { prisma } from "@/src/lib/prisma"
 
-function OrderPage({params}: {params: {category: string}}) {
-  console.log(params.category)
+async function getProducts(category: string){
+  const products = await prisma.product.findMany({
+    where:{
+      category: {
+        slug: category
+      }
+    }
+  })
+
+  return products
+}
+
+async function OrderPage({params}: {params: {category: string}}) {
+  
+  const products = await getProducts(params.category)
+
   return (
-    <div>OrderPage</div>
+    <>
+      <h1 className="text-2xl my-10">
+        Elige y personaliza tu pedido a continuacion
+      </h1>
+
+      <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4 items-start">
+        {products.map((product) => (
+          <ProductCard
+            key={product.id}
+            product={product}
+          />
+        ))}
+      </div>
+    </>
   )
 }
 
