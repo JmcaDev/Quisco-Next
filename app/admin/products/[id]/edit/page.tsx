0e18e9cf0e8b32a0dfc6f5d4ecd6ed659c72ae9d
JmcaDev <jmcavzla@gmail.com>
2025-02-19 19:49:46 -0400

    Editing product module

---
+++ app/admin/products/[id]/edit/page.tsx
@@ -0,0 +1,37 @@
+import EditProductForm from "@/components/products/EditProductForm"
+import ProductForm from "@/components/products/ProductForm"
+import Heading from "@/components/ui/Heading"
+import { prisma } from "@/src/lib/prisma"
+import { notFound } from "next/navigation"
+
+async function getProductById(id: number){
+    const product = await prisma.product.findUnique({
+        where: {
+            id
+        }
+    })
+    return product
+}
+
+async function EditProductsPage({params}: {params: {id: string}}) {
+    
+    const product = await getProductById(+params.id)
+    console.log(product)
+
+    if(!product){
+        notFound()
+    }
+
+    return (
+        <>
+            <Heading>Editar Producto: {product.name}</Heading>
+            <EditProductForm>
+                <ProductForm
+                    product = {product}
+                />
+            </EditProductForm>
+        </>
+    )
+}
+
+export default EditProductsPage
